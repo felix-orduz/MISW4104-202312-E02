d17984fec95d0c12df590f0a846257fcc329df02
Felix E. Orduz G <f.orduz@uniandes.edu.co>
2023-04-30 16:12:56 -0500

    test(app.component.spec.ts): add tests for header, footer and router outlet components in AppComponent

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,6 +2,11 @@ import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { HeaderComponent } from './shared/header/header/header.component';
+import { FooterComponent } from './shared/footer/footer/footer.component';
+import { RouterOutlet } from '@angular/router';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -16,16 +21,31 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  // it(`should have as title 'plantas-app'`, () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   const app = fixture.componentInstance;
-  //   expect(app.title).toEqual('plantas-app');
-  // });
+  it(`should have a header component`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    const personDebug: DebugElement = fixture.debugElement;
+    const h3Debug: DebugElement = personDebug.query(
+      By.directive(HeaderComponent)
+    );
+    expect(h3Debug).toBeTruthy();
+  });
 
-  // it('should render title', () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   fixture.detectChanges();
-  //   const compiled = fixture.nativeElement as HTMLElement;
-  //   expect(compiled.querySelector('.content span')?.textContent).toContain('plantas-app app is running!');
-  // });
+  it(`should have a footer component`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    const personDebug: DebugElement = fixture.debugElement;
+    const h3Debug: DebugElement = personDebug.query(
+      By.directive(FooterComponent)
+    );
+    expect(h3Debug).toBeTruthy();
+  });
+
+  it(`should have a router outlet component`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    const personDebug: DebugElement = fixture.debugElement;
+    const h3Debug: DebugElement = personDebug.query(By.directive(RouterOutlet));
+    expect(h3Debug).toBeTruthy();
+  });
 });
