7ada7f2314b61fe992a1a215f5ec7db05cb232ff
Felix E. Orduz G <f.orduz@uniandes.edu.co>
2023-04-30 20:40:07 -0500

    feat(list-plants): add PlantType enum and display total number of plants by type
    Add PlantType enum with two values: Interior and Exterior. Display the total number of plants of each type in the list-plants component.

--- src/app/plants/list-plants/list-plants.component.ts
+++ src/app/plants/list-plants/list-plants.component.ts
@@ -1,19 +1,29 @@
 import { Component } from '@angular/core';
-import { PlantService } from '../service/plant.service';
+import { PlantType } from '../enums/plant-type';
 import { Plant } from '../model/plant';
+import { PlantService } from '../service/plant.service';
 
 @Component({
   selector: 'list-plants',
   templateUrl: './list-plants.component.html',
-  styleUrls: ['./list-plants.component.scss']
+  styleUrls: ['./list-plants.component.scss'],
 })
 export class ListPlantsComponent {
   constructor(private plantService: PlantService) {}
   plantList: Plant[] = [];
+  totalInterior: number = 0;
+  totalExterior: number = 0;
 
   ngOnInit(): void {
     this.plantService.getAll().subscribe((plantList) => {
       this.plantList = plantList;
+      this.totalInterior = this.plantList.filter(
+        (planta) => planta.tipo === PlantType.Interior
+      ).length;
+
+      this.totalExterior = this.plantList.filter(
+        (planta) => planta.tipo === PlantType.Exterior
+      ).length;
     });
   }
 }
